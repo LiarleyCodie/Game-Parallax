2b144d90f1647d15c94c1c35111cd92b3d5c29e2
liarleycodie <liarley.personal@gmail.com>
2023-06-13 12:39:45 -0300

    feat: parallax working

--- main.js
+++ main.js
@@ -1,54 +1,70 @@
-import { Game } from 'phaser';
+import Phaser from 'phaser';
 
 const config = {
   type: Phaser.CANVAS,
   width: 576,
   height: 324,
+  pixelArt: true,
   scene: {
     preload: preload,
     create: create,
     update: update
   }
 };
-const game = new Phaser.Game(config);
+new Phaser.Game(config);
 
-function createImageObj(velocity) {
-  return {
-    image: null,
-    x: 0,
-    velocity: velocity
-  }
-}
-
-let cityLayer1 = createImageObj(0.1);
-let cityLayer2 = createImageObj(0.2);
-let cityLayer3 = createImageObj(0.3);
-let cityLayer4 = createImageObj(0.4);
+const parallaxLayers = [
+  { key: 'cityLayer1', velocity: 0.1 },
+  { key: 'cityLayer2', velocity: 0.2 },
+  { key: 'cityLayer3', velocity: 0.3 },
+  { key: 'cityLayer4', velocity: 0.4 }
+];
 
 function preload() {
   this.load.image('sky', 'assets/1.png');
-  this.load.image('cityLayer1', 'assets/2.png');
-  this.load.image('cityLayer2', 'assets/3.png');
-  this.load.image('cityLayer3', 'assets/4.png');
-  this.load.image('cityLayer4', 'assets/5.png');
+
+  parallaxLayers.forEach((layer, id) => {
+    this.load.image(layer.key, `assets/${id + 2}.png`);
+  })
 }
 function create() {
   this.add.image(0,0,'sky').setOrigin(0);
-  
-  cityLayer1.image = this.add.image(cityLayer1.x,0,'cityLayer1').setOrigin(0);
-  cityLayer2.image = this.add.image(cityLayer2.x,0,'cityLayer2').setOrigin(0);
-  cityLayer3.image = this.add.image(cityLayer3.x,0,'cityLayer3').setOrigin(0);
-  cityLayer4.image = this.add.image(cityLayer4.x,0,'cityLayer4').setOrigin(0);
+
+  parallaxLayers.forEach(layer => {
+    const { key, velocity } = layer;
+
+    const layerGroup = this.add.group();
+    layerGroup.enableBody = true;
+
+    const image1 = layerGroup.create(0,0,key).setOrigin(0);
+    const image2 = layerGroup.create(-image1.width, 0, key).setOrigin(0);
+    
+    layerGroup.velocity = velocity;
+
+    layer.image = layerGroup;
+  });
 }
 function update() {
-  cityLayer1.x += cityLayer1.velocity;
-  cityLayer2.x += cityLayer2.velocity;
-  cityLayer3.x += cityLayer3.velocity;
-  cityLayer4.x += cityLayer4.velocity;
+  const canvasWidth = this.game.config.width;
 
-  cityLayer1.image.setPosition(cityLayer1.x, 0);
-  cityLayer2.image.setPosition(cityLayer2.x, 0);
-  cityLayer3.image.setPosition(cityLayer3.x, 0);
-  cityLayer4.image.setPosition(cityLayer4.x, 0);
+  parallaxLayers.forEach(layer => {
+    const { image, velocity } = layer;
+
+    if (image.children.entries.length > 0) {
+      const firstImage = image.getFirstAlive();
+      if (firstImage.x <= -firstImage.width) {
+        const lastImage = image.getLast(true);
+        lastImage.x = firstImage.x + firstImage.width;
+      }
+    }
+
+    image.children.entries.forEach(image => {
+      image.x += velocity;
+
+      if (image.x > canvasWidth) {
+        image.x = -canvasWidth;
+      }
+    })
+  });
 }
 
